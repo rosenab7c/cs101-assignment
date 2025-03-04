27bb66fde394d0fbeae0f573cbe9c45e9af74815
rosenab7c <rosenab7c@gmx.com>
2025-03-04 13:53:45 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("Even\n");
+    } else {
+        printf("Odd\n");
+    }
+
+    return 0;
+}
