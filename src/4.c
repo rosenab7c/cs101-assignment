de8e5b63546e66aab76b7cc1e5e399a2560fac2e
rosenab7c <rosenab7c@gmx.com>
2025-03-06 16:36:37 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  int x = 5;
+  char* name = "John";
+
+  printf("Hello, %s!\n", name);
+  printf("The value of x is: %d\n", x);
+
+  return 0;
+}
